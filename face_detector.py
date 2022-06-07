1167bf1bc8e0328c6b2b025f795f71bd37bd3fb5
saiful-aromax <saiful.aromax@gmail.com>
2022-06-07 09:33:58 +0600

    update

--- face_detector.py
+++ face_detector.py
@@ -2,8 +2,7 @@ import cv2
 
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
+
 img = cv2.imread('images/RDJ.jpg')
 cv2.imshow('Clever Programmer Face Detector', img)
 cv2.waitKey()
-
-print("This is Aromax")
