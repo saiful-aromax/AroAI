3581a9c3dff09500efbc1841350d784515cc22df
saiful-aromax <saiful.aromax@gmail.com>
2022-06-18 17:27:08 +0600

    update

--- mnist.py
+++ mnist.py
@@ -1,11 +1,22 @@
 import tensorflow as tf
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+
+
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        accuracy = logs.get('accuracy')
+        if(accuracy > 0.9):
+            print("\nReached 90% accuracy so cancelling training!")
+            self.model.stop_training = True
+
+
+callbacks = myCallback()
+
 
 mnist = tf.keras.datasets.fashion_mnist
 
 (training_images, training_labels),  (test_images, test_labels) = mnist.load_data()
 
-plt.imshow(test_images[0])
 # plt.imshow(test_images[0])
 
 
@@ -16,15 +27,15 @@ test_images = test_images/255.0
 # print('training images:', training_images[0])
 
 model = tf.keras.models.Sequential([tf.keras.layers.Flatten(), tf.keras.layers.Dense(
-    1024, activation=tf.nn.relu), tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+    512, activation=tf.nn.relu), tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
 
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
-
-model.fit(training_images, training_labels, epochs=5)
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(training_images, training_labels, epochs=5, callbacks=[callbacks])
 
 model.evaluate(test_images, test_labels)
 
 classifications = model.predict(test_images)
 
-print(classifications[0])
-print(test_labels[0])
+print(classifications[47])
+print(test_labels[47])
