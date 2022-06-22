3525fac053b5ef4ee8f5092c1c511c374f531bbc
saiful-aromax <saiful.aromax@gmail.com>
2022-06-22 18:53:18 +0600

    update

--- convolution.py
+++ convolution.py
@@ -16,34 +16,31 @@ i_transformed = np.copy(i)
 size_x = i_transformed.shape[0]
 size_y = i_transformed.shape[1]
 
-#filter = [ [0, 1, 0], [1, -4, 1], [0, 1, 0]] 
-filter = [ [-1, -2, -1], [0, 0, 0], [1, 2, 1]]
+#filter = [ [0, 1, 0], [1, -4, 1], [0, 1, 0]]
+filter = [[-1, -2, -1], [0, 0, 0], [1, 2, 1]]
 #filter = [ [-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]
 
-weight  = 1
+weight = 1
 
-for x in range(1,size_x-1):
-  for y in range(1,size_y-1):
-      output_pixel = 0.0
-      output_pixel = output_pixel + (i[x - 1, y-1] * filter[0][0])
-      output_pixel = output_pixel + (i[x, y-1] * filter[0][1])
-      output_pixel = output_pixel + (i[x + 1, y-1] * filter[0][2])
-      output_pixel = output_pixel + (i[x-1, y] * filter[1][0])
-      output_pixel = output_pixel + (i[x, y] * filter[1][1])
-      output_pixel = output_pixel + (i[x+1, y] * filter[1][2])
-      output_pixel = output_pixel + (i[x-1, y+1] * filter[2][0])
-      output_pixel = output_pixel + (i[x, y+1] * filter[2][1])
-      output_pixel = output_pixel + (i[x+1, y+1] * filter[2][2])
-      output_pixel = output_pixel * weight
-      if(output_pixel<0):
-        output_pixel=0
-      if(output_pixel>255):
-        output_pixel=255
-      i_transformed[x, y] = output_pixel
-    
+for x in range(1, size_x-1):
+    for y in range(1, size_y-1):
+        output_pixel = 0.0
+        output_pixel = output_pixel + (i[x - 1, y-1] * filter[0][0])
+        output_pixel = output_pixel + (i[x, y-1] * filter[0][1])
+        output_pixel = output_pixel + (i[x + 1, y-1] * filter[0][2])
+        output_pixel = output_pixel + (i[x-1, y] * filter[1][0])
+        output_pixel = output_pixel + (i[x, y] * filter[1][1])
+        output_pixel = output_pixel + (i[x+1, y] * filter[1][2])
+        output_pixel = output_pixel + (i[x-1, y+1] * filter[2][0])
+        output_pixel = output_pixel + (i[x, y+1] * filter[2][1])
+        output_pixel = output_pixel + (i[x+1, y+1] * filter[2][2])
+        output_pixel = output_pixel * weight
+        output_pixel = 0 if output_pixel < 0 else (255 if output_pixel > 255 else output_pixel)
+        i_transformed[x, y] = output_pixel
+
 
 plt.gray()
 plt.grid(False)
 plt.imshow(i_transformed)
-#plt.axis('off')
+# plt.axis('off')
 plt.show()
