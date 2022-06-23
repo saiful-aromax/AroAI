e55ab3c518c72ee90c4c2740aba141dbd095f9ac
saiful-aromax <saiful.aromax@gmail.com>
2022-06-23 13:24:32 +0600

    update

--- conv2.py
+++ conv2.py
@@ -15,6 +15,7 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(128, activation='relu'),
     tf.keras.layers.Dense(10, activation='softmax')
 ])
+
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 model.summary()
