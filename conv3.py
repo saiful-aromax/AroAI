90935bab581aa9e38c5ee39ad7ea25f31778443e
saiful-aromax <saiful.aromax@gmail.com>
2022-06-28 20:56:41 +0600

    update

--- conv3.py
+++ conv3.py
@@ -10,7 +10,7 @@ model = tf.keras.models.Sequential([
   tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(28, 28, 1)),
   tf.keras.layers.MaxPooling2D(2, 2),
   tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
-  tf.keras.layers.MaxPooling2D(2,2),
+  tf.keras.layers.MaxPooling2D(2, 2),
   tf.keras.layers.Flatten(),
   tf.keras.layers.Dense(128, activation='relu'),
   tf.keras.layers.Dense(10, activation='softmax')
@@ -21,25 +21,25 @@ model.fit(training_images, training_labels, epochs=5)
 test_loss, test_accuracy = model.evaluate(test_images, test_labels)
 # print ('Test loss: {}, Test accuracy: {}'.format(test_loss, test_accuracy*100))
 
-f, axarr = plt.subplots(3,4)
-FIRST_IMAGE=0
-SECOND_IMAGE=23
-THIRD_IMAGE=28
+f, axarr = plt.subplots(3, 4)
+FIRST_IMAGE = 0
+SECOND_IMAGE = 23
+THIRD_IMAGE = 28
 CONVOLUTION_NUMBER = 6
 
 layer_outputs = [layer.output for layer in model.layers]
 activation_model = tf.keras.models.Model(inputs = model.input, outputs = layer_outputs)
 
 
-for x in range(0,4):
+for x in range(0, 4):
     f1 = activation_model.predict(test_images[FIRST_IMAGE].reshape(1, 28, 28, 1))[x]
-    axarr[0,x].imshow(f1[0, : , :, CONVOLUTION_NUMBER], cmap='inferno')
-    axarr[0,x].grid(False)
+    axarr[0, x].imshow(f1[0, : , :, CONVOLUTION_NUMBER], cmap='inferno')
+    axarr[0 ,x].grid(False)
     f2 = activation_model.predict(test_images[SECOND_IMAGE].reshape(1, 28, 28, 1))[x]
-    axarr[1,x].imshow(f2[0, : , :, CONVOLUTION_NUMBER], cmap='inferno')
-    axarr[1,x].grid(False)
+    axarr[1, x].imshow(f2[0, : , :, CONVOLUTION_NUMBER], cmap='inferno')
+    axarr[1, x].grid(False)
     f3 = activation_model.predict(test_images[THIRD_IMAGE].reshape(1, 28, 28, 1))[x]
-    axarr[2,x].imshow(f3[0, : , :, CONVOLUTION_NUMBER], cmap='inferno')
-    axarr[2,x].grid(False)
+    axarr[2, x].imshow(f3[0, : , :, CONVOLUTION_NUMBER], cmap='inferno')
+    axarr[2, x].grid(False)
     
 sigma = "Aromax"
