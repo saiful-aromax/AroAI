c0c56dd9f4b3b675c13aa8fde5939ec90ca180e1
saiful-aromax <saiful.aromax@gmail.com>
2022-07-03 00:26:40 +0600

    conv4

--- conv3.py
+++ conv3.py
@@ -6,6 +6,8 @@ training_images=training_images.reshape(60000, 28, 28, 1)
 training_images=training_images / 255.0
 test_images = test_images.reshape(10000, 28, 28, 1)
 test_images=test_images / 255.0
+print(test_labels[:100])
+
 model = tf.keras.models.Sequential([
   tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(28, 28, 1)),
   tf.keras.layers.MaxPooling2D(2, 2),
