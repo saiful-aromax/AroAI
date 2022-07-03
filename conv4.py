d7c03b7941f75751cb9ec4fa024fe26eefae3949
saiful-aromax <saiful.aromax@gmail.com>
2022-07-03 19:13:18 +0600

    update

--- conv4.py
+++ conv4.py
@@ -21,12 +21,9 @@ print(scissors_files[:10])
 
 pic_index = 2
 
-next_rock = [os.path.join(rock_dir, fname)
-for fname in rock_files[pic_index-2:pic_index]]
-next_paper = [os.path.join(paper_dir, fname)
-for fname in paper_files[pic_index-2:pic_index]]
-next_scissors = [os.path.join(scissors_dir, fname)
-for fname in scissors_files[pic_index-2:pic_index]]
+next_rock = [os.path.join(rock_dir, fname) for fname in rock_files[pic_index-2:pic_index]]
+next_paper = [os.path.join(paper_dir, fname) for fname in paper_files[pic_index-2:pic_index]]
+next_scissors = [os.path.join(scissors_dir, fname) for fname in scissors_files[pic_index-2:pic_index]]
 
 for i, img_path in enumerate(next_rock+next_paper+next_scissors):
     # print(img_path)
