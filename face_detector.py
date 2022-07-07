ff707253b7ebdfa8f9b97172e26505b57a8633ef
saiful-aromax <saiful.aromax@gmail.com>
2022-07-08 02:28:57 +0600

    update

--- face_detector.py
+++ face_detector.py
@@ -4,5 +4,9 @@ trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 
 img = cv2.imread('images/RDJ.jpg')
-cv2.imshow('Clever Programmer Face Detector', img)
+grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Detecct faces
+face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+
+cv2.imshow('Clever Programmer Face Detector', grayscaled_img)
 cv2.waitKey()
