ad05e41c5d9c7a42b25f311ad70ec064c97a4f92
saiful-aromax <saiful.aromax@gmail.com>
2022-07-09 03:55:12 +0600

    update

--- fd_video.py
+++ fd_video.py
@@ -1,17 +1,15 @@
 import cv2
-from random import randrange
 
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+webcam = cv2.VideoCapture(0)
 
-
-img = cv2.imread('images/multi3.jpg')
-grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# Detecct faces
-face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
-for (x, y, w, h) in face_coordinates:
+while True:
+    is_success_read, frame = webcam.read()
     
-    cv2.rectangle(img, (x, y), ((x + w), (y+ h)), (randrange(64, 256), randrange(64, 256), randrange(64, 256)), 2)
-
-
-cv2.imshow('Clever Programmer Face Detector', img)
-cv2.waitKey()
+    grayscaled_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    face_coordinates = trained_face_data.detectMultiScale(grayscaled_frame)
+    for (x, y, w, h) in face_coordinates:
+        cv2.rectangle(frame, (x, y), ((x + w), (y+ h)), (0, 255, 0), 2)
+        
+    cv2.imshow('Face Detection', frame)
+    cv2.waitKey(1)
