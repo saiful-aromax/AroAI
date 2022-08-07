d77238508745ddbfbd2ae113d870739133fa89cd
saiful-aromax <saiful.aromax@gmail.com>
2022-08-07 12:45:15 +0600

    update

--- fd_image.py
+++ fd_image.py
@@ -4,7 +4,7 @@ from random import randrange
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 
-img = cv2.imread('images/multi3.jpg')
+img = cv2.imread('images/multi2.jpg')
 grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # Detecct faces
 face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
