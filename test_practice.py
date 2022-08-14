079a7c4f6fca821a5542956b6aceab598458b1fd
saiful-aromax <saiful.aromax@gmail.com>
2022-08-14 22:53:19 +0600

    update

--- test_practice.py
+++ test_practice.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-A = np.random.rand(100)
-# print(A)
-plt.hist(A)
-plt.show()
+A = np.random.rand(10,2)
+B = np.arange(100)
+# plt.hist(A, bins=100)
+# plt.show()
+print(A)
