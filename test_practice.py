6fc06ce0a123aba309ea1467b738f84493e9e79c
saiful-aromax <saiful.aromax@gmail.com>
2022-08-15 23:42:29 +0600

    update

--- test_practice.py
+++ test_practice.py
@@ -1,8 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-A = np.random.rand(10,2)
-B = np.arange(100)
+A = np.random.rand(100)
+B = np.random.randn(1000)
+C = np.arange(100).reshape(2, 50)
+D = np.random.permutation(np.arange(30))
+E = np.arange(30, 70, 3)
+F = np.argwhere(C == 51)
 # plt.hist(A, bins=100)
 # plt.show()
-print(A)
+# plt.hist(B, bins=100)
+# plt.show()
+# plt.hist(C)
+# plt.show()
+
+print(F)
