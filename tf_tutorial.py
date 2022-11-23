5af700ec45a54c55f2b2329ccb9e094af0359c69
saiful-aromax <saiful.aromax@gmail.com>
2022-11-24 02:21:04 +0600

    update

--- tf_tutorial.py
+++ tf_tutorial.py
@@ -1,14 +1,24 @@
 import tensorflow as tf
-import matplotlib.pyplot as plt
 
-(training_images, training_labels), (test_images, test_labels) = tf.keras.datasets.fashion_mnist.load_data()
+mnist = tf.keras.datasets.fashion_mnist
 
-training_images  = training_images / 255.0
-test_images = test_images / 255.0
+(training_images, training_labels) ,  (test_images, test_labels) = mnist.load_data()
 
-print(training_labels[0])
-print(training_images[0])
-plt.imshow(training_images[0])
-# plt.axis('Off')
-plt.show()
-sigma = "Aromax"
+training_images = training_images/255.0
+test_images = test_images/255.0
+
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
+                                    tf.keras.layers.Dense(1024, activation=tf.nn.relu),
+                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+
+model.compile(optimizer = 'adam',
+              loss = 'sparse_categorical_crossentropy')
+
+model.fit(training_images, training_labels, epochs=5)
+
+model.evaluate(test_images, test_labels)
+
+classifications = model.predict(test_images)
+
+print(classifications[0])
+print(test_labels[0])
