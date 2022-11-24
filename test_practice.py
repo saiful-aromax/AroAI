b6f60edb66f05045f4d89cfdee91004b2a672236
saiful-aromax <saiful.aromax@gmail.com>
2022-11-24 15:14:49 +0600

    update

--- test_practice.py
+++ test_practice.py
@@ -3,7 +3,12 @@ import matplotlib.pyplot as plt
 
 A = np.random.rand(100)
 B = np.random.randn(1000)
+B_C = np.arange(25)
 C = np.arange(100).reshape(4, 25)
+C_D = C[1:]
+C_D1 = C[:1]
+C_D2 = C[:2]
+C_D3 = C_D2[0]
 D = np.random.permutation(np.arange(30))
 E = np.arange(30, 70, 3)
 F = np.argwhere(C == 51)
@@ -13,5 +18,6 @@ F = np.argwhere(C == 51)
 # plt.show()
 # plt.hist(C)
 # plt.show()
+G = C[1:4]
 
 print(C[1:4])
